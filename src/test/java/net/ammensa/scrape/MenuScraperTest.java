ca3e9a98ec9b01110f5c854c633960343e14c6ab
Alessandro <ale.ricchiuti@hotmail.it>
2019-01-23 20:26:34 +0100

    Added integration testing configuration.

---
+++ src/test/java/net/ammensa/scrape/MenuScraperTest.java
@@ -0,0 +1,13 @@
+package net.ammensa.scrape;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenuScraperTest {
+
+    @Test
+    void scrapePdfMenuUrl() {
+        assertTrue(true);
+    }
+}
