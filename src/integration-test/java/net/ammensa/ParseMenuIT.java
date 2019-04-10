f95cddf641fd4789282b734dcb0773ea9dcf48aa
aleric <ale.ricchiuti@hotmail.it>
2019-04-10 09:58:19 +0200

    Added first parsing integration test.

---
+++ src/integration-test/java/net/ammensa/ParseMenuIT.java
@@ -0,0 +1,50 @@
+package net.ammensa;
+
+import net.ammensa.cron.MenuUpdate;
+import net.ammensa.entity.Menu;
+import net.ammensa.repository.MenuRepository;
+import net.ammensa.utils.HttpDownload;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.util.ReflectionTestUtils;
+import reactor.core.publisher.Mono;
+
+import java.nio.file.Files;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ParseMenuIT {
+
+
+    @Autowired
+    private MenuUpdate menuUpdate;
+    @Autowired
+    private MenuRepository menuRepository;
+    @Mock
+    HttpDownload httpDownloadMock;
+
+    @Test
+    public void findAndParseMenu() throws Exception {
+
+        /* TODO: usare un webserver di test */
+        byte[] menuBytes = Files.readAllBytes(new ClassPathResource("menu/MERCOLEDI' 20 FEBBRAIO 2019 PRANZO.pdf").getFile().toPath());
+        Mono<byte[]> monoMenuBytes = Mono.just(menuBytes);
+        Mockito.when(httpDownloadMock.download(Mockito.any())).thenReturn(monoMenuBytes);
+        ReflectionTestUtils.setField(menuUpdate, "httpDownload", httpDownloadMock);
+
+        menuUpdate.updateMenu().subscribe((a) -> {
+
+            Optional<Menu> retrievedMenu = menuRepository.retrieve();
+
+            assertTrue(retrievedMenu.isPresent());
+            assertFalse(retrievedMenu.get().getFirstCourses().isEmpty());
+        });
+    }
+}
+
