d0a5d1ff5a9ac27f5110691af77255c7593dca7e
aleric <ale.ricchiuti@hotmail.it>
2019-04-17 09:30:43 +0200

    Added cron to avoid application sleeping on heroku.

---
+++ src/main/java/net/ammensa/cron/ApplicationWakeUp.java
@@ -0,0 +1,26 @@
+package net.ammensa.cron;
+
+import net.ammensa.utils.HttpDownload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component
+public class ApplicationWakeUp {
+
+    private static final Logger LOGGER = Logger.getLogger(ApplicationWakeUp.class.getName());
+
+    @Autowired
+    private HttpDownload httpDownload;
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Scheduled(cron = "0 */10 * * * *")
+    public void wakeUpApplication() {
+        LOGGER.info("starting wake-up");
+        httpDownload.download("localhost:" + serverPort).subscribe((a) -> LOGGER.info("wake-up complete"));
+    }
+}
