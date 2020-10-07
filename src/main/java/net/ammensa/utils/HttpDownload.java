8518d2ff13468078b314659bc48b9b8c296aaf67
aleric <ale.ricchiuti@hotmail.it>
2020-10-08 01:34:55 +0200

    Increased the max bytes to buffer limit.

--- src/main/java/net/ammensa/utils/HttpDownload.java
+++ src/main/java/net/ammensa/utils/HttpDownload.java
@@ -3,6 +3,7 @@ package net.ammensa.utils;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -13,13 +14,18 @@ public class HttpDownload {
 
     private static final Logger LOGGER = Logger.getLogger(HttpDownload.class.getName());
 
-    private static final WebClient WEB_CLIENT = WebClient.create();
+    private final WebClient web_client = WebClient.builder()
+            .exchangeStrategies(ExchangeStrategies.builder()
+                    /* 10 MB */
+                    .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(1024 * 1024 * 10))
+                    .build())
+            .build();
 
     public Mono<byte[]> download(String resourceUrl) {
 
         LOGGER.info("starting download from " + resourceUrl);
 
-        return WEB_CLIENT
+        return web_client
                 .method(HttpMethod.GET)
                 .uri(resourceUrl)
                 .exchange()
